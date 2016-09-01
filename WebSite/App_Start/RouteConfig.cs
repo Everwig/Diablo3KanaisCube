b2d0730ee9fd3fb0bb17bc380c224ee9eefed04a
Gabor Porteleki <gabor_porteleki@epam.com>
2016-09-01 16:25:47 +0200

    Updated default route.config

--- WebSite/App_Start/RouteConfig.cs
+++ WebSite/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace WebSite
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Weapons", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
